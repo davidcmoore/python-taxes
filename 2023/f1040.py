783a02c7826aa3ac0e33834b17d7e2204ce4c919
Stefano Mihai Canta <cantastefano@gmail.com>
2025-11-23 17:11:56 -0600

    Requested changes

--- 2023/f1040.py
+++ 2023/f1040.py
@@ -131,7 +131,7 @@ class F1040(Form):
             f.comment['12'] = 'Standard deduction'
             f['12'] = std
 
-        if f['11']-f['12'] < f.SS_MAX or (f['11']-f['12'] < 2*f.SS_MAX and inputs['status'] == FilingStatus.JOINT):
+        if f['11']-f['12'] < f.BRACKET_LIMITS[inputs['status']][3]:
             f8995 = f.addForm(F8995(inputs, f, sd, sse))
             if f8995.mustFile():
                 f['13'] = f8995['15']
@@ -154,7 +154,6 @@ class F1040(Form):
         # Compute line s3_1 now because it's needed by AMT
         f.comment['s3_1'] = 'Foreign Tax Paid'
         foreign_tax = inputs.get('foreign_tax', 0)
-        # assert(foreign_tax < 300 or (foreign_tax < 600 and inputs['status'] == FilingStatus.JOINT))
         if foreign_tax:
             f['s3_1'] = foreign_tax
 
--- 2024/f8995.py
+++ 2024/f8995.py
@@ -1,21 +1,16 @@
 from form import Form, FilingStatus
 
 class F8995(Form):
-    """Form 8995A, Qualified Business Income Deduction Simplified Computation"""
+    """Form 8995, Qualified Business Income Deduction Simplified Computation"""
     def __init__(f, inputs, f1040, sched_d, sse):
         super(F8995, f).__init__(inputs)
 
-        if inputs['status'] == FilingStatus.JOINT:
-            f['2']  = sse[0]['3'] - sse[0]['13'] if sse[0].mustFile else 0
-            f['2'] += sse[1]['3'] - sse[1]['13'] if sse[1].mustFile else 0
-        else:
-            f['2']  = sse['3'] - sse['13'] if sse.mustFile else 0
-        f['3'] = inputs.get('qualified_business_net_carryforward')
-        f['4'] = max(0, f['2'] - f['3'])
+        f['2'] = f1040['s1_3'] - (f1040.rowsum(['s1_1[567]']) or 0)
+        f['4'] = max(0, f['2'] + f['3'])
         f['5'] = f['4'] * 0.20
         f['6'] = inputs.get('section_199A_dividends')
         f['7'] = inputs.get('qualified_REIT_net_carryforward')
-        f['8'] = max(0, f['6'] - f['7'])
+        f['8'] = max(0, f['6'] + f['7'])
         f['9'] = f['8'] * 0.2
         f['10'] = f['5'] + f['9']
         f['11'] = f1040['11'] - f1040['12']
@@ -25,6 +20,8 @@ class F8995(Form):
         f['15'] = min(f['10'], f['14'])
         if f['15']:
             f.must_file = True
+        f['16'] = min(f['2'] + f['3'], 0.)
+        f['17'] = min(f['6'] + f['7'], 0.)
 
     def title(self):
         return 'Form 8995'
